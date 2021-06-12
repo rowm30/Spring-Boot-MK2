fb432a3f946bf52816d6ac838183c0bf31a66f67
rowm30 <rowm30@gmail.com>
2021-06-12 19:47:15 +0530

    On_My_Game

--- src/main/java/com/example/mk2/Mk2Application.java
+++ src/main/java/com/example/mk2/Mk2Application.java
@@ -2,12 +2,16 @@ package com.example.mk2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Mk2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Mk2Application.class, args);
     }
+
+    
 
 }
